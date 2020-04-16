3fe2b4424e45b446e721fdde0d33156d68d4bbf4
Ruan Aragao <aragao.ruan@gmail.com>
2020-04-16 20:07:10 -0300

    adding methods HTTP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({message:'Hello World'});
+app.get('/projecs', (req, res) => {
+  return res.json([
+    'Project 1',
+    'Project 2',
+  ]);
+});
+
+app.post('/projects', (req, res) => {
+  return res.json([
+    'Project 1',
+    'Project 2',
+    'Project 3',
+  ]);
+});
+
+app.put('/project/:id', (req, res) => {
+  return res.json([
+    'Project 1',
+    'Project 4',
+    'Project 3',
+  ]);
+});
+
+app.delete('/peojects/:id', () => {
+  return res.json([
+    'Project 4',
+    'Project 3',
+  ]);
 })
 
 app.listen(3333, () => {
